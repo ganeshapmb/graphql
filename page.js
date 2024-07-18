f42d9738cee07161b9674a6909794776961ede5f
ganeshapmb <63532509+ganeshapmb@users.noreply.github.com>
2024-07-18 07:30:56 -0700

    Add files via upload

--- page.js
+++ page.js
@@ -1,96 +1,12 @@
-'use client'
-import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import Image from 'next/image'
-
-const Slider = () => {
+import ProductOne from "@/app/components/ProductOne";
+export default async function Page({ params }) {
+    console.warn("params",params)
+    const id = params.slug;
+    console.warn("id", id)
     return (
-        <div>
-            <Swiper
-                // install Swiper modules
-                modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
-                spaceBetween={15}  // Adjust the space between slides
-                // slidesPerView={5}  // Initially, show 1 slide (single image) on small screens
-                // centeredSlides={true}
-                navigation
-                pagination={{ clickable: true }}
-                scrollbar={{ draggable: true }}
-                onSwiper={(swiper) => console.log(swiper)}
-                onSlideChange={() => console.log('slide change')}
-                autoplay={{ delay: 3000 }}
-                breakpoints={{
-                    576: {
-                      slidesPerView: 1,
-                      spaceBetween: 20,
-                    },
-                    600: {
-                      slidesPerView: 2,
-                      spaceBetween: 20,
-                    },
-                    700: {
-                        slidesPerView: 3,
-                        spaceBetween: 20,
-                      },
-                    1024: {
-                      slidesPerView: 4,
-                      spaceBetween: 20,
-                    },
-                  }}
-                className="mt-5"
-            >
-                <div className='text-center'>
-                    <SwiperSlide>
-                        <div className="swiper-inner">
-                            <Image src="https://images.pexels.com/photos/7319273/pexels-photo-7319273.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width={2250}
-                                height={1390} />
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="swiper-inner">
-                            <Image src='https://images.pexels.com/photos/4050288/pexels-photo-4050288.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' width={2250}
-                                height={1390} />
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="swiper-inner">
-                            <Image src="https://images.pexels.com/photos/7319273/pexels-photo-7319273.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width={2250}
-                                height={1390} />
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="swiper-inner">
-                            <Image src='https://images.pexels.com/photos/4050289/pexels-photo-4050289.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' width={2250}
-                                height={1390} />
-                        </div>
-
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="swiper-inner">
-                            <Image src='https://images.pexels.com/photos/3779747/pexels-photo-3779747.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' width={2250}
-                                height={1390} />
-                        </div>
-
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className="swiper-inner">
-                            <Image src="https://images.pexels.com/photos/7319273/pexels-photo-7319273.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width={2250}
-                                height={1390} />
-                        </div>
-                    </SwiperSlide>
-
-                </div>
-
-            </Swiper>
-        </div>
-
+        <>
+        <ProductOne  productId={params.slug} />
+        </>
     )
 }
-
-export default Slider
